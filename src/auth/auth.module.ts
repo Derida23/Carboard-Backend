124705b352bd17c847a0d4cc503b4199527f38c8
Arian Derida <IA9268@PID-PROJECT-IA9268.local>
2024-06-27 13:37:21 +0700

    feat(auth): create register endpoint

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      property: 'user',
+      session: false,
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+    PrismaModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { hashPassword } from 'utils/hash-password.util';
+import { ApiResponse } from 'interface/response.type';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) { }
+  
+  async create(payload: CreateAuthDto) {
+    const { email, password, ...rest } = payload;
+
+    // Checking existing email
+    const existingUser = await this.prisma.users.findUnique({ where: { email } });
+    if (existingUser) {
+      throw new ConflictException('Email is already in use');
+    }
+
+    // Encrypted password
+    const hashedPassword = await hashPassword(password);
+    await this.prisma.users.create({
+      data: { ...rest, email, password: hashedPassword },
+    });
+
+    return this.buildResponse("Successfully registered");
+  }
+
+  private buildResponse(message: string, access_token = null): ApiResponse<string> {
+    return {
+      message: message,
+      data: access_token,
+      statusCode: 200,
+    };
+  }
+}
