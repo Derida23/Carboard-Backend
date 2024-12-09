922055f735087401664853fde93d9f1f28e6b765
Arian Derida <IA9268@PID-PROJECT-IA9268.local>
2024-12-09 15:16:51 +0700

    fix(guard): update role guard message

--- src/auth/roles/roles.guard.ts
+++ src/auth/roles/roles.guard.ts
@@ -1,5 +1,9 @@
-
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from './roles.enum';
 import { ROLES_KEY } from './roles.decorator';
@@ -7,7 +11,7 @@ import { ROLES_KEY } from './roles.decorator';
 //Role Guard
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) { }
+  constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
@@ -17,9 +21,14 @@ export class RolesGuard implements CanActivate {
     if (!requiredRoles) {
       return true;
     }
-    //get the user object from the request object
     const { user } = context.switchToHttp().getRequest();
-    //check if the user has the required roles if the user has the required roles, return true else return false
-    return requiredRoles.some((role) => user.roles === role);
+    const acceptRole = requiredRoles.some((role) => user.roles === role);
+
+    if (!acceptRole) {
+      throw new ForbiddenException(
+        `Access denied. Your role '${user.roles}' does not grant permission to access this resource.`,
+      );
+    }
+    return true;
   }
 }
